82a41d0e29362f85f12b94bc3067b70019c543a8
Gaurav Kumar <gauravkumar15021999@gmail.com>
2024-12-07 17:15:05 +0530

    Refector to use list for social links

--- src/app/pages/gui/components/header/header.component.ts
+++ src/app/pages/gui/components/header/header.component.ts
@@ -1,13 +1,20 @@
-import { Component, HostListener, inject, Input, signal } from '@angular/core';
+import { Component, HostListener, inject, input, signal } from '@angular/core';
 import {MatIconModule } from '@angular/material/icon';
 import {MatButtonModule} from '@angular/material/button';
 import {MatToolbarModule} from '@angular/material/toolbar';
 import {MatDrawer, MatSidenavModule} from '@angular/material/sidenav';
 import { FixedPositionDirective, Positions } from '../../../../directives/fixed-position.directive';
 import {MatTooltipModule, TooltipPosition} from '@angular/material/tooltip';
-import { faGithub } from '@fortawesome/free-brands-svg-icons';
 import { AppTheme, ThemeService } from '../../../../services/theme.service';
 import { RouterLink } from '@angular/router';
+
+interface SocialLink {
+  tooltip: string;
+  tooltipPosition: TooltipPosition
+  url: string;
+  iconType: 'svgIcon' | 'fontIcon';
+  icon: string;
+}
 
 @Component({
   selector: 'app-header',
@@ -18,30 +25,70 @@ import { RouterLink } from '@angular/router';
   styleUrl: './header.component.scss'
 })
 export class HeaderComponent {
+  drawer = input.required<MatDrawer>();
+  isSmallScreen = input.required<boolean>();
+
   isDarkMode = signal(false);
-  faGithub = faGithub;
-  isSmallScreen = signal(false);
   positions = Positions;
+
+  socialLinks: SocialLink[] = [
+    { 
+      tooltip: 'Switch to Terminal Portfolio', 
+      tooltipPosition: 'left',
+      url: 'https://gauravkumar1502.github.io/Terminal-Portfolio/', 
+      iconType: 'fontIcon', 
+      icon: 'code' 
+    },
+    { 
+      tooltip: 'GitHub',
+      tooltipPosition: 'left',
+      url: 'https://github.com/Gauravkumar1502', 
+      iconType: 'svgIcon', 
+      icon: 'github' 
+    },
+    { 
+      tooltip: 'LinkedIn',
+      tooltipPosition: 'left', 
+      url: 'https://www.linkedin.com/in/gauravkumar15', 
+      iconType: 'svgIcon', 
+      icon: 'linkedin' 
+    },
+    { 
+      tooltip: 'Twitter',
+      tooltipPosition: 'left', 
+      url: 'https://x.com/Gauravkuma_r', 
+      iconType: 'svgIcon', 
+      icon: 'twitter' 
+    },
+    { 
+      tooltip: 'HackerRank',
+      tooltipPosition: 'left',
+      url: 'https://www.hackerrank.com/profile/gauravkumar15021', 
+      iconType: 'svgIcon',
+      icon: 'hacker_rank' 
+    },
+    { 
+      tooltip: 'Codepen',
+      tooltipPosition: 'left', 
+      url: 'https://codepen.io/gauravkumar1502', 
+      iconType: 'svgIcon', 
+      icon: 'codepen' 
+    }
+  ];
+
   email: string = 'gaurav.kumar.deve@gmail.com';
-  params = {
+  emailParams = {
     subject: 'Hello',
-    body: 'Hello Gaurav,'
+    body: 'Hi Gaurav,'
   };
-  @Input() drawer!: MatDrawer;
   private themeService = inject(ThemeService);
+
   constructor() {
     this.isDarkMode.set(this.themeService.isDarkMode('gui'));
-    this.isSmallScreen.set(window.innerWidth < 768);
   }
   
   get currentIcon(): string {
     return this.isDarkMode() ? 'dark_mode' : 'light_mode';
-  }
-
-  @HostListener('window:resize', ['$event'])
-  onResize(event: Event): void {
-    const width = (event.target as Window).innerWidth;
-    this.isSmallScreen.set(width < 768);
   }
   
   toggleTheme(): void {
